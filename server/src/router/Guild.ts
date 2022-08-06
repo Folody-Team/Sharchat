5030f7512fbe759fd42248e792da8b5994d521f7
LamHanoi10 <77109479+LamHanoi10@users.noreply.github.com>
2022-08-06 12:16:13 +0700

    Update Guild.ts

--- server/src/router/Guild.ts
+++ server/src/router/Guild.ts
@@ -1,5 +1,9 @@
 import {Router} from "express"
+import {CreateGuild, DeleteGuild} from "../controller/Guild"
 
 const GuildRouter = Router()
+
+GuildRouter.post('/create', CreateGuild)
+GuildRouter.delete('/delete', DeleteGuild)
 
 export default GuildRouter
