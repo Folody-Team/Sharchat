df521f3a5255c796756ba79fb682582b164ed1fd
LamHanoi10 <77109479+LamHanoi10@users.noreply.github.com>
2022-08-06 17:53:03 +0700

    Added edit guild function

--- server/src/controller/Guild.ts
+++ server/src/controller/Guild.ts
@@ -90,3 +90,61 @@ export const DeleteGuild = async (req: Request, res: Response) => {
 		})
 	}
 }
+
+export const EditGuild = async (req: Request, res: Response) => {
+	const {id, name, description} = req.body
+	if (!id) {
+		res.status(400).json({
+			message: 'Missing required fields',
+		})
+		return
+	}
+
+	try {
+		const guild = await GuildModel.findById(id)
+		if (!guild) {
+			res.status(400).json({
+				message: 'Guild not found',
+			})
+			return
+		}
+		const result = await MemberUtil.CheckPermissions(
+			res.locals.userId,
+			guild._id,
+			{
+				isOwner: true,
+			}
+		)
+		if (
+			!result.isOwner &&
+			!result.permissions.includes('admin') &&
+			!result.permissions.includes('server_manager')
+		) {
+			res.status(403).send({
+				message: "Requested member doesn't have permission to edit",
+			})
+			return
+		}
+
+		if (
+			(name && typeof name == 'string') ||
+			(description && typeof description == 'string')
+		) {
+			if (name && typeof name == 'string') guild.name = name
+			if (description && typeof description == 'string')
+				guild.description = description
+
+			await guild.save()
+		}
+
+		res.status(200).send({
+			message: 'Guild edited',
+			guild,
+		})
+	} catch (error) {
+		console.log(error)
+		res.status(500).json({
+			message: 'Error editing guild',
+		})
+	}
+}
